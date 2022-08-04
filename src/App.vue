d9aa9d27a793375ae48bdcab9444d5c006fdacbd
Divakar-Saravade <saravade.dd@gmail.com>
2022-08-04 15:34:09 +0530

    my first vue code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Hello World!" />
+  <HelloWorld msg="Hello Vue!" /> -->
+  <FirstVue />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import FirstVue from './components/FirstVue.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    FirstVue,
   }
 }
 </script>
---
+++ src/components/FirstVue.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  // component options
+}
+</script>
+
+<template>
+  <h1>First Vue!</h1>
+</template>
